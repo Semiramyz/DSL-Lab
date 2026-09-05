9379fcfba329c07618c2c1ec2c79e3fe08e3e686
Semiranyz <js0207367@gmail.com>
2026-09-04 22:21:09 -0500

    V2

--- DSL-LAB/obj/Debug/net10.0/DSL-LAB.AssemblyInfo.cs
+++ DSL-LAB/obj/Debug/net10.0/DSL-LAB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSL-LAB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc9606b3cae3c85e56ba5ba09f13349282f62d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80a529e5b88926d462b74adf43d49a17c181bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSL-LAB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSL-LAB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
